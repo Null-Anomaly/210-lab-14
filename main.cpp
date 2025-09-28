56f0e780a88f36faa41dae9df4f602802d1ceec6
L B <lbryant019@insite.4cd.edu>
2025-09-28 15:04:53 -0700

    10 minute commit

--- main.cpp
+++ main.cpp
@@ -5,6 +5,27 @@ IDE used: Visual Studio Code*/
 #include <iomanip>
 using namespace std;
 
+class Color
+{
+    public:
+    int getRed() {return red;}
+    int getBlue() {return blue;}
+    int getGreen() {return green;}
+    int setRed(int r) {red = r;}
+    int setBlue(int b) {blue = b;}
+    int setGreen(int g) {green = g;}
+    void print()
+    {
+        cout << "";
+    }
+
+    private:
+    int green;
+    int red;
+    int blue;
+
+};
+
 int main()
 {
 
